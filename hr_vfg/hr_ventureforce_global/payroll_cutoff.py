911513442737b1122f759d83c8f275c5f17826a1
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2026-05-04 12:51:56 +0500

    chore: remove cut off date

--- hr_vfg/hr_ventureforce_global/payroll_cutoff.py
+++ hr_vfg/hr_ventureforce_global/payroll_cutoff.py
@@ -21,40 +21,6 @@ def _resolve_document_date(doc):
 
 @frappe.whitelist()
 def enforce_payroll_cutoff(doc, method=None):
-	# Restrict only new records; updates/cancellation of existing records remain allowed.
-	if not doc.is_new():
-		return
-
-	cutoff_date = frappe.db.get_single_value("V HR Settings", "payroll_cut_off_date")
-	if not cutoff_date:
-		return
-
-	cutoff_date = getdate(cutoff_date)
-	today = getdate(nowdate())
-	document_date = _resolve_document_date(doc)
-
-	# Block new records once the system date has crossed the configured cut off date.
-	if today > cutoff_date:
-		frappe.throw(
-			_(
-				"Cannot create {0} because today's date {1} is after Payroll Cut Off Date {2}. "
-				"Update V HR Settings to continue."
-			).format(
-				frappe.bold(doc.doctype),
-				frappe.bold(today.strftime("%Y-%m-%d")),
-				frappe.bold(cutoff_date.strftime("%Y-%m-%d")),
-			)
-		)
-
-	# Also prevent forward-dated entries beyond cut off before crossing date.
-	if document_date > cutoff_date:
-		frappe.throw(
-			_(
-				"Cannot create {0} for date {1} because Payroll Cut Off Date is {2}. "
-				"Update V HR Settings to continue."
-			).format(
-				frappe.bold(doc.doctype),
-				frappe.bold(document_date.strftime("%Y-%m-%d")),
-				frappe.bold(cutoff_date.strftime("%Y-%m-%d")),
-			)
-		)
+	# Payroll cut-off enforcement is disabled. Keep the hook in place so it can be
+	# re-enabled later without changing the event bindings again.
+	return
